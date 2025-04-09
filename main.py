0194ae59d3413375da33252d7947ccc7877391da
Neeran Bari <barineer@umich.edu>
2025-04-09 13:24:06 -0400

    implemented Open Weather API key and printed corresponding JSON

--- main.py
+++ main.py
@@ -21,3 +21,17 @@ else:
 
 def build_url(movie):
     return f"http://www.omdbapi.com/?apikey={API_KEY}&t={movie.replace(' ', '+')}"
+
+open_api_key = "3c4d479363893dd3a68aa9d11f95be22"
+param3 = "units=Metric"
+zipcode = "48103"
+base = "https://api.openweathermap.org/data/2.5/weather"
+param1 = f"appid={open_api_key}"
+param2 = f"zip={zipcode}"
+request = f"{base}?{param1}&{param2}&{param3}"
+
+print(request)
+page = requests.get(request)
+print(page.status_code) #to check if the request is successful
+weather  = page.json()    #turn the information into a json object
+print(json.dumps(weather, indent = 2))
